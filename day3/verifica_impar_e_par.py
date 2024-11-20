6e475aafcc74f1777b8cfb80e9bdcc45884377d0
alestanalves <alestanalves@gmail.com>
2024-11-20 19:28:57 -0300

    adicionando dia 3 condicional

---
+++ day3/verifica_impar_e_par.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite o numero: "))
+
+if numero % 2 == 0: #se essa condicional
+    #nao for satisfeita
+    print(f"Este numero eh impar -> {numero}")
+else:
+    print("Este numero nao eh par")
