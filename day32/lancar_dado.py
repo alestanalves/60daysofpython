fbf244c676e356d0d9b9c4c0294649391c8f7ce1
alestanalves <alestanalves@gmail.com>
2024-12-31 16:28:29 -0300

    add day 33 and day 32

--- day32/lancar_dado.py
+++ day32/lancar_dado.py
@@ -1,13 +1,14 @@
-import random 
+from random import randint
 
 def lancar_dado():
     """
-    Simula o lancamento de um dado de 6 faces.
+    Simular o lancamento de um dado de 1 a 6
+
     Return:
-        int: Um numero inteiro entre 1 a 6
+        int: Um numero aleatorio de 1 a 6
     """
 
-    return random.randint(1, 6)
+    return randint(1, 6)
 
 if __name__ == "__main__":
-    print(f"O resultado do dado eh: {lancar_dado()}")
+    print(f"O resultado do dado foi: {lancar_dado()}")
